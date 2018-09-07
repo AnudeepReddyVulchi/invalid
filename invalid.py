f56dde0c5a1cfc2391184fe41493bdc874bfdcc3
AnudeepReddyVulchi <42571727+AnudeepReddyVulchi@users.noreply.github.com>
2018-09-07 11:46:12 +0530

    Update invalid.py

--- invalid.py
+++ invalid.py
@@ -2,8 +2,8 @@ str = raw_input()
 ab = ['a','e','i','o','u','A','E','I','O','U']
 list = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
 if (str in ab):
-	print("vowel")
+	print("Vowel")
 elif (str in list):
-	print("consonant")
+	print("Consonant")
 else:
 	print("invalid")
