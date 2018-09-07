8474ce42db5db007756e0c7e686750399896b4ce
AnudeepReddyVulchi <42571727+AnudeepReddyVulchi@users.noreply.github.com>
2018-09-07 11:48:35 +0530

    Update invalid.py

--- invalid.py
+++ invalid.py
@@ -1,7 +1,7 @@
 str = raw_input()
-ab = ['a','e','i','o','u','A','E','I','O','U']
+cd = ['a','e','i','o','u','A','E','I','O','U']
 list = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z']
-if (str in ab):
+if (str in cd):
 	print("Vowel")
 elif (str in list):
 	print("Consonant")
